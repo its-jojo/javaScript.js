72e017c3d6831d5b1440ad9076af8ebf07a9552e
Jyoti Mehra <jyotimehra32991@gmail.com>
2025-05-02 06:54:08 +0000

    02variable.js

---
+++ basic/02variable.js
@@ -0,0 +1,21 @@
+const accountId= 3242
+let accountEmail= "jyotimehra@gmail.com"
+var accountPassword= "3224"
+accountCity = "Nainital"
+
+// accountId= 2  //not allowed
+
+console.log(accountId)
+
+accountEmail ="himanshu@gmail.com"
+accountPassword= "212121"
+accountCity="delhi"
+
+/*
+prefer do no use var
+because of issue in the block case
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
