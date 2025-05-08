1cb43b20de5d4ad9aaadd59eb57cdc06e70b65cc
Jyoti Mehra <jyotimehra32991@gmail.com>
2025-05-08 15:37:20 +0000

    03datatype.js

---
+++ basic/03datatype.js
@@ -0,0 +1,18 @@
+let name = "jyoti"
+let age = 20
+let inLoggedIn= false
+
+
+//number => 2 to power 53
+//bigInt 
+// string=>""
+// boolean =>true, false
+// null => standalone value
+// undefine =>
+// symbol
+
+// object
+
+console.log(typeof "jyoti")
+console.log(typeof null)  //object
+console.log(typeof undefined) //undefined
