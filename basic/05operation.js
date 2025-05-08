61359e4e8ad1c2d2bc4d114a4b88ad860346088c
Jyoti Mehra <jyotimehra32991@gmail.com>
2025-05-08 16:07:37 +0000

    05operation.js

---
+++ basic/05operation.js
@@ -0,0 +1,26 @@
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+
+console.log(2+2)
+console.log(2-2)
+console.log(2**2)
+console.log(2*2)
+console.log(2%3)
+
+
+let str1 = "jyoti"
+let str2= "mehra"
+let str3= str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(2+"1")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+
+let gameCounter = 100
+gameCounter++;  //postfix
+console.log(gameCounter);
