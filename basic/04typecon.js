db8cf55442fa2aae0b01ba77ef928bff6e11d8f6
Jyoti Mehra <jyotimehra32991@gmail.com>
2025-05-08 15:52:49 +0000

    04typecon.js

---
+++ basic/04typecon.js
@@ -0,0 +1,33 @@
+let score ="33"
+console.log(typeof score)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+
+
+let score1 =null
+console.log(typeof score1)
+let valueInNumber2 = Number(score1)
+console.log(typeof valueInNumber2)
+
+// "33"=>33
+// "33abc" => NaN
+// true =>1, false =>0
+
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn= Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+
+//  1=> true
+// 0=>false
+// ""=>false
+// "jyoti" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
